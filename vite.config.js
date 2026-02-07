fac12a82bfab10a1a02d15a3710723d27c990617
zohabiya123 <zohabiyanaeem@gmail.com>
2026-02-08 00:27:37 +0500

    Update project with latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  base: './', // ye relative path bana deta hai
 })
