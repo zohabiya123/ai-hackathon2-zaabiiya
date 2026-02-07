da73c36fda31c4be6bc25562fff27af96231114a
zohabiya123 <zohabiyanaeem@gmail.com>
2026-02-08 00:47:08 +0500

    Fix App import case

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App.jsx"; // ✅ correct path
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
